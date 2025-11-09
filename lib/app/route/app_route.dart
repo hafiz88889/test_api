47be5b93e77326d9ccac1d77dca567f612d1f74e
MD Hafizur Rahman <minhajuddin50013@gmail.com>
2025-11-10 00:12:08 +0600

    push

--- lib/app/route/app_route.dart
+++ lib/app/route/app_route.dart
@@ -1,26 +1,68 @@
 import 'dart:ui';
 
 import 'package:go_router/go_router.dart';
+import 'package:test_new_app/app/main/module/home_screen/view/home_page.dart';
 import 'package:test_new_app/app/main/module/onboarding_screen/view/onboarding_page.dart';
 import 'package:test_new_app/app/main/module/splash_screen/view/splash_screen.dart';
 import 'package:test_new_app/app/route/app_path.dart';
 
-class AppRouter{
+class AppRouter {
   AppRouter._();
-  static final GoRouter appRouter=GoRouter(
+
+  static final GoRouter appRouter = GoRouter(
     initialLocation: Routes.SPLASH_SCREEN,
-      routes: [
-        GoRoute(
-          path: Routes.INITIAL,
-          builder: (context, state)=> const SplashScreen(),
-        ),
-        GoRoute(
-            path: Routes.ONBOARDING_SCREEN,
-        builder: (context, state)=>OnboardingPage(),
-          routes: [
-
-          ]
-        )
-
-  ]);
+    routes: [
+      GoRoute(
+        path: Routes.SPLASH_SCREEN,
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: Routes.ONBOARDING_SCREEN,
+        builder: (context, state) => OnboardingPage(),
+      ),
+    ],
+  );
 }
+
+//
+// class AppRouter {
+//   AppRouter._();
+//
+//   static final GoRouter appRouter = GoRouter(
+//     initialLocation: Routes.SPLASH_SCREEN,
+//     routes: [
+//       GoRoute(
+//         path: Routes.INITIAL,
+//         builder: (context, state) => const SplashScreen(),
+//       ),
+//       GoRoute(
+//         path: Routes.ONBOARDING_SCREEN,
+//         builder: (context, state) => OnboardingPage(),
+//         routes: []
+//       ),
+//     ]
+//   );
+//
+// }
+
+//
+// class AppRouter{
+//   AppRouter._();
+//   static final GoRouter appRouter=GoRouter(
+//     initialLocation: Routes.SPLASH_SCREEN,
+//       routes: [
+//         GoRoute(
+//           path: Routes.INITIAL,
+//           builder: (context, state)=> const SplashScreen(),
+//         ),
+//         GoRoute(
+//             path: Routes.ONBOARDING_SCREEN,
+//         builder: (context, state)=>OnboardingPage(),
+//           routes: [
+//
+//           ]
+//         )
+//
+//   ]
+//   );
+// }
